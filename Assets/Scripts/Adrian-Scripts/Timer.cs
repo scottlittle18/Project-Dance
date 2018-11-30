6332a9ddb862d90fc9f6d31f1696b46cdf862155
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-11-30 15:25:32 -0600

    Timer Script

---
+++ Assets/Scripts/Adrian-Scripts/Timer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField]
+    bool IsRunning;
+
+    [SerializeField]
+    float EndTime;
+
+    [SerializeField]
+    float CurrentTime;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        IsRunning = false;
+
+	}
+
+    public void StartTimer()
+    {
+
+        IsRunning = true;
+    }
+
+    public void StopTimer()
+    {
+
+        IsRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+
+        EndTime = 0;
+        CurrentTime = 0;
+
+    }
+
+    public void ResetTimer(float NewEndTime)
+    {
+
+        EndTime = NewEndTime;
+        CurrentTime = 0;
+
+    }
+
+    void FixedUpdate()
+    {
+		
+        if (IsRunning)
+        {
+
+            CurrentTime += Time.fixedDeltaTime;
+            if (CurrentTime >= EndTime)
+            {
+
+                IsRunning = false;
+
+            }
+
+        }
+
+	}
+
+}
