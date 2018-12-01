88a88cb53264582e7bd0687a5776ef9fe276375b
Scott Little <scott.little@loop.colum.edu>
2018-12-01 15:44:26 -0600

    Push

---
+++ Assets/Scripts/Scott-Scripts/SceneLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    string currentScene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    public void TestLevel()
+        {
+            SceneManager.LoadScene("Test Level");
+        }
+    
+        public void LoadTitleScreen()
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+
+        public void Level1()
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+
+        public void Level2()
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+
+        public void Level3()
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+
+        public void Level4()
+        {
+            SceneManager.LoadScene("Level 4");
+        }
+
+        public void Retry()
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+
+        public void Credits()
+        {
+            SceneManager.LoadScene("Credits");
+        }
+
+        public void QuitGame()
+        {        
+            Application.Quit();
+        }
+
+}
